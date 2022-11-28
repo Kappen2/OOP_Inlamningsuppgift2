844ef60721ad77435723bfde3dc9f966564e43c9
Stefan Franzén <stefra90@live.se>
2022-11-28 19:36:11 +0100

    Working on search function

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class ReadFile {
     protected static String info;
     public void ReadCustomersFile() {
-        Scanner input = new Scanner(System.in);
-        Scanner fileScan = new Scanner("src/customers.txt");
-        fileScan.useDelimiter(",");
+
         String temp;
         String searchWord;
         String paymentDate ="";
         boolean isCustomer = false;
         try (BufferedReader bufIn = new BufferedReader(new FileReader("src/customers.txt"))){
+            Scanner input = new Scanner(System.in);
+            Scanner fileScan = new Scanner("src/customers.txt");
+            fileScan.useDelimiter(",");
             while(true) {
                 System.out.println("Skriv in ett namn eller personnummer: ");
-            if(input.hasNextInt()) {
+            if(input.hasNextInt()) { //läser in ett personnummer och omvandlar till String
                 searchWord = String.valueOf(input.nextInt());
                 break;
             }
-            else if (input.hasNext()) {
-                searchWord = input.useDelimiter("\\s*,\\s*").nextLine();
+            else if (input.hasNext()) { //läser in ett namn
+                searchWord = input.next();
                 if (searchWord != null)
                     break;
             }
             }
-            while ((temp = bufIn.readLine()) != null) {
-
+            while ((temp = bufIn.readLine()) != null) { //kollar om kunden finns i filen
                 if(temp.equalsIgnoreCase(searchWord)){
                     info = temp;
-                    paymentDate = bufIn.readLine();
+                    paymentDate = fileScan.nextLine();
                     isCustomer = true;
                     break;
                 }
             }
-            if (isCustomer) {
+            if (isCustomer) { // kollar om kunden har betalat
                 LocalDate payDate = LocalDate.parse(paymentDate);
                 LocalDate currentDate = LocalDate.now();
-                if(payDate.plusDays(365).isAfter(currentDate)) {
+                if(payDate.plusDays(365).isAfter(currentDate)) { //skriver till en fil om kunden har betalat
                     WriteFile visitors = new WriteFile();
                     visitors.createVisitorFile();
                     System.out.println("Kunden är en medlem\n"+temp+ "\n"+paymentDate);
@@ -47,6 +48,9 @@ public class ReadFile {
             else System.out.println("Personen du sökte finns inte i databasen");
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (NoSuchElementException e1){
+            System.out.println("Du gjorde en felaktig sökning");
+            e1.printStackTrace();
         }
     }
 }
